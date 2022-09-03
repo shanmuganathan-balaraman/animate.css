cbe46c98107f633e1625ad491521b0ae014e7ea4
Shanmuganathan B <Shanmuganathan.balaraman@freshworks.com>
2022-09-03 15:00:08 +0530

    update nodejs version

--- .github/workflows/commit.sh
+++ .github/workflows/commit.sh
@@ -10,12 +10,6 @@ git remote add pusher "${remote_repo}"
 # push to github
 git checkout ${BRANCH_NAME}
 git add -A
-git reset -- yarn.lock # to ignore yarn lock file being commited
 git commit -m "${COMMIT_MESSAGE} -- auto build" --no-verify || exit 0
-
-# since yarn.lock file is not committed it will show as unstaged changes, which will thorw error in pull with rebase
-# to discard the same we are adding it to stash and dropping it
-# git stash
-# git stash drop 
-git pull pusher ${BRANCH_NAME}
+git pull --rebase pusher ${BRANCH_NAME}
 git push pusher ${BRANCH_NAME}
